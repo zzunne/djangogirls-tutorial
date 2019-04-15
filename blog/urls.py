175d0b3d3c9bf7e77364b24e0e6b35a812135475
zzunne <kjoon9236@gmail.com>
2019-04-15 16:01:12 +0900

    포스트 게시 전 미리보기로 블로그 글 저장하기

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.urls import path
 from . import views
 
@@ -6,4 +7,6 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
 ]
